284d970b2aa771625023a0e37b3aafbd15a24058
Andrew Bishop <was.bishop@icloud.com>
2023-03-16 17:53:14 -0400

    cursor
    adding cursor to follow mouse

---
+++ cs492project/Assets/Sprites/Cursor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+
+    public void Start() 
+    {
+        UnityEngine.Cursor.visible = false;
+    }
+
+    public void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(cursorPos.x + 0.40f, cursorPos.y - 0.40f);   
+    }
+    
+}

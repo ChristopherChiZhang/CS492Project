62f48086ab9786643b6f870da67e318f69da445b
Andrew Bishop <was.bishop@icloud.com>
2023-03-29 15:12:52 -0400

    adding end screen scoring

--- cs492project/Assets/Scripts/GameOverScript.cs
+++ cs492project/Assets/Scripts/GameOverScript.cs
@@ -2,9 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameOverScript : MonoBehaviour
 {
+
+    public TextMeshProUGUI scoreBreakdown;
+    public TextMeshProUGUI totalScoreText;
+
+    void Start() {
+
+        string scoreText = "";
+
+        int totalScores = PlayerPrefs.GetInt("totalScores");
+        int totalScore = 0;
+        
+        
+        for (int i = 0; i < totalScores; i++) {
+            string line = "";
+            int scoreNum = PlayerPrefs.GetInt("scoreNum" + i);
+            string scoreString = PlayerPrefs.GetString("scoreString" + i);
+            line += scoreNum + ": " + scoreString;
+            totalScore += scoreNum;
+            scoreText += line + System.Environment.NewLine;
+        }
+
+        scoreBreakdown.text = scoreText;
+        totalScoreText.text = "Total Score: " + totalScore;
+    }
+
+
 
     public void RestartGame()
     {

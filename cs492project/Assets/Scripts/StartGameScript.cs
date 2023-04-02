c7589525fc1ecd2625ffb89a1f3463a38ef06e41
Rick Liu <ricksliu.mail@gmail.com>
2023-04-01 23:21:20 -0400

    Game start popup

--- cs492project/Assets/Scripts/StartGameScript.cs
+++ cs492project/Assets/Scripts/StartGameScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,7 +11,4 @@ public class StartGameScript : MonoBehaviour
     {
         SceneManager.LoadScene("Homepage");
     }
-
-
-
 }
